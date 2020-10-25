eb4e1fe67a5bb4850c350211d73a5c755d6dbe29
pratx1906 <57968015+pratx1906@users.noreply.github.com>
2020-10-26 02:28:10 +0530

    All files needed .

---
+++ assign08/src/assign08/BinarySearchTree.java
@@ -0,0 +1,81 @@
+package assign08;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.NoSuchElementException;
+
+public class BinarySearchTree<Type extends Comparable<? super Type>> implements SortedSet<Type>{
+
+	@Override
+	public boolean add(Type item) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends Type> items) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean contains(Type item) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsAll(Collection<? extends Type> items) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Type first() throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Type last() throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean remove(Type item) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeAll(Collection<? extends Type> items) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ArrayList<Type> toArrayList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
